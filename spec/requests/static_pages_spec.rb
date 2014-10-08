efcd8e77e40cad97f9248c6776de0ffb0b3b88bd
pkb2917 <pk_becker@hotmail.com>
2014-10-08 01:06:45 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,13 @@ describe "Static Pages" do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
         end
-        it "should have the title 'Home'" do
+        it "should have the base title" do
             visit '/static_pages/home'
-            expect(page).to have_title("#{base_title} | Home")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+        end
+        it "should not have a custom page title" do
+            visit '/static_pages/home'
+            expect(page).not_to have_title('| Home')
         end
     end
   
@@ -22,7 +26,11 @@ describe "Static Pages" do
         end
         it "should have the title 'Help'" do
             visit '/static_pages/help'
-            expect(page).to have_title("#{base_title} | Help")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+        end
+        it "should not have a custom page title" do
+            visit '/static_pages/help'
+            expect(page).not_to have_title('| Help')
         end
     end
     
@@ -33,7 +41,11 @@ describe "Static Pages" do
         end
         it "should have the title 'About Us'" do
             visit '/static_pages/about'
-            expect(page).to have_title("#{base_title} | About Us")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+        end
+        it "should not have a custom page title" do
+            visit '/static_pages/about'
+            expect(page).not_to have_title('| About Us')
         end
     end
     
@@ -44,7 +56,11 @@ describe "Static Pages" do
         end
         it "should have the title 'Contact'" do
             visit '/static_pages/contact'
-            expect(page).to have_title("#{base_title} | Contact")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+        end
+        it "should not have a custom page title" do
+            visit '/static_pages/contact'
+            expect(page).not_to have_title('| Contact')
         end
     end
 end
